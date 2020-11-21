4a36b4b588bc8f26556c45e7ee3df53164a0543e
Chaz Roman <chazroman2@gmail.com>
2020-11-21 17:49:22 -0500

    Random qote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,16 @@
+import random
+
+last = 13
+rnd = random.randint(0, last)
+
 def primary():
    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+f = open("quotes.txt")
+quotes = f.readlines()
+f.close()
 
-  #print(quotes)
+print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
